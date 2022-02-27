e633b1ed54c7dd6e9ca8b6a7240fcfb5edeb2f29
ankitsinghrana13 <ak9897432538@gmail.com>
2022-02-28 06:25:06 +0800

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class swapping
+    {
+
+        public static void Main(String[] args)
+        {
+
+            Console.WriteLine("Enter the first number");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the second value");
+            int b = Convert.ToInt32(Console.ReadLine());
+            a = a - b;
+            b = a + b;
+            a = b - a;
+
+            Console.WriteLine("a=" + a);
+            Console.WriteLine("b=" + b);
+
+        }
+
+
+    }
+}
